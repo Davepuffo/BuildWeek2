0423a720353f622038199f71eabba723972524cf
Davepuffo <123974851+Davepuffo@users.noreply.github.com>
2023-03-21 19:47:25 +0100

    buildWeek2

--- assets/JS/homepage.js
+++ assets/JS/homepage.js
@@ -2,10 +2,10 @@ let URL_rock = 'https://striveschool-api.herokuapp.com/api/deezer/search?q=rock'
 let URL_pop = 'https://striveschool-api.herokuapp.com/api/deezer/search?q=pop'
 let i = 0;
 
-displayDataRock = function(data) {
+displayDataRock = function (data) {
     let rowReference = document.getElementById('row_rock')
     rowReference.innerHTML = ''
-    for (i=0; i<data.length && i<6; i++) {
+    for (i = 0; i < data.length && i < 6; i++) {
         let newCol = document.createElement('div');
         newCol.classList.add('col-md-2');
         newCol.innerHTML = `
@@ -15,7 +15,7 @@ displayDataRock = function(data) {
                                     <div class="play">
                                         <i class="bi bi-play-fill"></i>
                                     </div>
-                                    <h4>${data[i].artist.name}</h4>
+                                    <a href="./artist_page.html?id=${data[i].artist.id}"><h4>${data[i].artist.name}</h4></a>
                                     <p>${data[i].album.title}</p>
                                 </div>
                             </div>
@@ -25,10 +25,10 @@ displayDataRock = function(data) {
     }
 }
 
-displayDataPop = function(data) {
+displayDataPop = function (data) {
     let rowReference = document.getElementById('row_pop')
     rowReference.innerHTML = ''
-    for (i=0; i<data.length && i<6; i++) {
+    for (i = 0; i < data.length && i < 6; i++) {
         let newCol = document.createElement('div');
         newCol.classList.add('col-md-2');
         newCol.innerHTML = `
@@ -38,7 +38,7 @@ displayDataPop = function(data) {
                                     <div class="play">
                                         <i class="bi bi-play-fill"></i>
                                     </div>
-                                    <h4>${data[i].artist.name}</h4>
+                                    <a href="./artist_page.html"><h4>${data[i].artist.name}</h4></a>
                                     <p>${data[i].album.title}</p>
                                 </div>
                             </div>
@@ -49,41 +49,41 @@ displayDataPop = function(data) {
 }
 
 
-const getDataRock = function() {
+const getDataRock = function () {
     fetch(URL_rock)
-.then((response)=>{
-    if(response.ok){
-        return response.json()
-    }else {
-        return new Error('Errore nella gestione della chiamata')
-    }
-})
-.then((data)=>{
-displayDataRock(data.data)
-})
-.catch((err) => {
-    console.log(err)
-})
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                return new Error('Errore nella gestione della chiamata')
+            }
+        })
+        .then((data) => {
+            displayDataRock(data.data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 }
 
 getDataRock()
 
 
-const getDataPop = function() {
+const getDataPop = function () {
     fetch(URL_pop)
-.then((response)=>{
-    if(response.ok){
-        return response.json()
-    }else {
-        return new Error('Errore nella gestione della chiamata')
-    }
-})
-.then((data)=>{
-displayDataPop(data.data)
-})
-.catch((err) => {
-    console.log(err)
-})
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                return new Error('Errore nella gestione della chiamata')
+            }
+        })
+        .then((data) => {
+            displayDataPop(data.data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 }
 
 getDataPop()

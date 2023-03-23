831ed433126a417922e92d26a44c494b40844188
Cujba Tiberiu Stefan <123594218+CujbaTiberiu@users.noreply.github.com>
2023-03-23 15:23:40 +0100

    miglioramenti

--- assets/JS/Album_page.js
+++ assets/JS/Album_page.js
@@ -5,26 +5,35 @@ console.log('Id', albumId)
 
 const showAlbum = function (album) {
     let albumBody = document.getElementsByClassName('album__body')[0]
+    albumBody.style.backgroundImage = `url(${album.cover_big}), linear-gradient(180deg, rgba(0, 0, 0, 0.3967517401392111) 0%, rgba(0, 0, 0, 1) 25%); `
     console.log(albumBody)
-    albumBody.innerHTML = `<div><img class="album__img shadow-lg img-fluid" src="${album.cover_medium}" alt="${album.title}">
+    albumBody.innerHTML = `<div><img class="album__img shadow img-fluid ms-1 my-2 border" src="${album.cover_medium}" alt="${album.title}">
 </div>
-<div class="ms-2 mt-3">
-    <p class="album__album fw-bolder">Album</p>
-    <h2 class=" album__name fw-bolder">${album.title}</h2>
-    <div class="artist__info d-flex">
-        <div class="d-flex">
+    <div class="ms-2 mt-sm-5 mt-md-0 mt-lg-0 mt-xl-0 album__info">
+    <div class="mt-5">
+    <p class="album__album fw-bolder mb-0 ms-1 mt-4">Album</p>
+    <h2 class=" album__name fw-bolder ms-1">${album.title}</h2>
+    </div>
+    <div class="artist__info d-flex ms-2">
+        <div class="d-flex mt-4 fs-6 art__inf">
             <img class="rounded-circle mx-1" src="${album.artist.picture_small}" alt="${album.artist.name}">
-            <p>${album.artist.name}&nbsp&#183&nbsp</p>
-            <p class="anno__ril">${album.release_date}&nbsp&#183&nbsp</p>
-            <p class="nr__canzoni">${album.nb_tracks} songs, <span class="opacity-50 small">${Math.floor(album.duration / 60)}&nbspminutes</span>
+            <p class="fw-bold">${album.artist.name}</p>
+            <p class="anno__ril">&nbsp&#183&nbsp${album.release_date}&nbsp&#183&nbsp</p>
+            <p class="nr__canzoni">${album.nb_tracks} songs, <span class="opacity-50 small">${formatDuration(album.duration)}&nbspminutes</span>
             </p>
         </div>
-        <p class="album__annoR d-sm-none">Album - <span>${album.release_date}</span></p>
+        <p class="album__annoR d-sm-none opacity-50">Album&nbsp&#183&nbsp<span>${album.release_date}</span></p>
     </div>
 </div>`
+
+    function formatDuration(durationInSeconds) {
+        const minutes = Math.floor(durationInSeconds / 60);
+        const seconds = durationInSeconds % 60;
+        const secondsString = seconds.toFixed(0).padStart(2, '0');
+        return `${minutes}:${secondsString}`;
+    }
+
 }
-
-
 
 const showSongs = function (songs) {
     let playList = document.getElementById('allSongs')
@@ -34,19 +43,24 @@ const showSongs = function (songs) {
         playList.innerHTML += `<div class="col-7">
         <div class="artist__title d-flex align-items-center">
             <div>
-                <p class="text-white opacity-75">${songIndex}</p>
+                <p class="text-white opacity-75 song__index">${songIndex}</p>
             </div>
+         <div class="d-flex align-items-center justify-content-between">
             <div class="d-flex flex-column ms-3">
-                <p class="mb-1 text-white opacity-75">${song.artist.name}</p>
-                <p class="text-white opacity-50">${song.title}</p>
+                <p class="mb-1 text-white opacity-75">${song.title}</p>
+                <p class="text-white opacity-50 small">${song.artist.name}</p>
             </div>
+            <div>
+            <i class="bi bi-three-dots-vertical mx-2 text-white opacity-75 d-none"></i>
+            </div>
+         </div>
         </div>
     </div>
-    <div class="col-3 text-white opacity-75">
+    <div class="col-4 text-white opacity-75">
         <p class="ripro__numb">${song.rank}</p>
     </div>
-    <div class="col-2 text-white opacity-75">
-        <p class="song__time">${Math.floor(song.duration / 60)}&nbspminutes</p>
+    <div class="col-1 text-white opacity-75 song__time">
+        <p class="song__time">${Math.floor(song.duration / 60)}</p>
     </div>`
         playList.appendChild(songElement)
     })
